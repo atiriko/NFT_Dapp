9768dc3c00e2be7ad3c570fa270231e4880ab567
Atahan Sahlan <atahansahlan@gmail.com>
2021-11-13 23:57:03 +0000

    Fixed Stars

--- build/main.js
+++ build/main.js
@@ -1,17 +1,14 @@
 
 var canvas;
-
 class Star {
   constructor(x, y, brightness, isRed) {
     this.x = x;
     this.y = y;
-    this.isRed = isRed
+    this.isRed = isRed;
     this.brightness = brightness;
-    noStroke()
-
+    noStroke();
     ellipse(this.x,this.y,2,2);
 
-    //draw();
   }
   draw(){
     if (this.isRed){
@@ -26,36 +23,24 @@ class Star {
 
 var xs = [];
 var ys = [];
+let star = [];
 
 function setup(){
   if (windowWidth<800){
-    canvas = createCanvas(windowWidth,10100)
+    canvas = createCanvas(windowWidth,4000)
 
   }else{
+
     canvas = createCanvas(windowWidth,4900)
 
   }
   canvas.position(0,0)
   canvas.style('z-index', '-2')
  
-  // background(255);
   for(let i = 0; i < 5000; i++){
+    star.push(new Star(random(0,windowWidth),random(0,canvas.height),map(sin(i+frameCount*0.02),-1,1,50,255), i%2 == 0));
+    star[i].draw();
 
-    // map(sin(frameCount)-1,1,0,255)
-    // console.log(sin(frameCount))
-    xs.push(random(0,windowWidth))
-    ys.push(random(0,canvas.height))
-
-  }
-  for(let i = 0; i < 5000; i++){
-    // map(sin(frameCount)-1,1,0,255)
-
-
-    let star
-    star = new Star(xs[i],ys[i],map(sin(i+frameCount*0.02),-1,1,50,255), i%2 == 0);
-   star.draw();
-
-    // ellipse(xs[i],ys[i],2,2);
   }
 
 }
@@ -65,41 +50,38 @@ function setup(){
 function windowResized(){
   
   if (windowWidth<800){
-    resizeCanvas(windowWidth,10100)
-
+    resizeCanvas(windowWidth,4000)
   }else{
     resizeCanvas(windowWidth,4900)
-
   }
-  xs=[]
-  ys=[]
-  for(let i = 0; i < 5000; i++){
 
-    // map(sin(frameCount)-1,1,0,255)
-    // console.log(sin(frameCount))
-    xs.push(random(0,windowWidth))
-    ys.push(random(0,canvas.height))
-
-  }
+  star = []
 
   for(let i = 0; i < 5000; i++){
-    // map(sin(frameCount)-1,1,0,255)
-
-
-    let star
-    star = new Star(xs[i],ys[i],map(sin(i+frameCount*0.02),-1,1,50,255), i%2 == 0);
-   star.draw();
-
-    // ellipse(xs[i],ys[i],2,2);
+    star.push( new Star(random(0,windowWidth),random(0,canvas.height),map(sin(i+frameCount*0.02),-1,1,50,255), i%2 == 0));
+    star[i].draw();
   }
+
 }
 
-function draw(){
-  // fill(map(sin(frameCount*0.02),-1,1,50,255)); 
-//  background(0);
-  
 
+function draw(){
+  if (windowWidth<800){
+    if(window.scrollY > 3000 && window.scrollY < 6000){
+      canvas.position(0,2900)
   
+    }else if(window.scrollY > 6000){
+      canvas.position(0,5900)
+  
+    }else{
+      canvas.position(0,0)
+  
+    }
+
+  }
+
+
+
 
 }
 
